2853b6b8cec13ca5875ec39437e727ed05d7f523
Pankaj Navale <98072841+pankajnavale24@users.noreply.github.com>
2022-08-13 18:25:41 +0530

    Add files via upload

---
+++ Assignment_13_Q7.c
@@ -0,0 +1,30 @@
+//7. Write a recursive function to calculate HCF of two numbers
+#include <stdio.h>
+
+int hcf(int, int);
+
+int main()
+{
+    int a, b, result;
+
+    printf("Enter the two numbers to find their HCF: ");
+    scanf("%d%d", &a, &b);
+    result = hcf(a, b);
+    printf("The HCF of %d and %d is %d.\n", a, b, result);
+}
+
+int hcf(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+        {
+            return hcf(a - b, b);
+        }
+        else
+        {
+            return hcf(a, b - a);
+        }
+    }
+    return a;
+}
