59015d84e55a0abebd1f27c6f4b11c9b3393a646
Pankaj Navale <98072841+pankajnavale24@users.noreply.github.com>
2022-08-13 18:22:51 +0530

    Add files via upload

---
+++ Assignment_13_Q6.c
@@ -0,0 +1,17 @@
+//Write a recursive function to calculate factorial of a given number
+#include<stdio.h>
+int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
